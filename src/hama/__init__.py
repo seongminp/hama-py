b61a9046548689f2f2c2d4a1425207dd94977cc4
Seongmin Park <llov0708@gmail.com>
2021-12-06 00:00:46 +0900

    [Fix] Fix circular dependency

--- src/hama/__init__.py
+++ src/hama/__init__.py
@@ -1,5 +1,6 @@
-from .g2p import Phoneminizer
 from .init import init
 from .jamo import assemble, chosungs, disassemble, jongsungs, joongsungs
 from .state_machine import *
 from .string_search import *
+
+from .g2p import Phonemizer
